6ab0661d5f852b371ea9006351e9a3ac077dbc46
MunJitso <72764494+MunJitso@users.noreply.github.com>
2023-08-23 17:36:43 +0100

    Add files via upload

---
+++ content.js
@@ -0,0 +1,10 @@
+setInterval(function () {
+    var div = document.querySelector('div.x1hc1fzr.x1unhpq9.x6o7n8i');
+    if (div) {
+        // Remove the div from its parent node
+        div.parentNode.removeChild(div);
+        clearInterval();
+    }
+}, 1);
+
+
